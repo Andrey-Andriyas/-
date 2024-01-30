e42bdd0c03ec9c05a41ce24e3f8d5b9da20da3e8
Андрей Андрияс <andriyas@mail.ru>
2024-01-30 12:39:21 +0700

    Delete lesson_7 directory

--- lesson_7/pages/CartPage_task_3.py
+++
@@ -1,9 +0,0 @@
-from selenium.webdriver.common.by import By
-
-class CartPage:
-
-    def __init__(self, browser):
-        self.driver = browser
-
-    def click_checkout(self):
-        self.driver.find_element(By.CSS_SELECTOR, "#checkout").click()
